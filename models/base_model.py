059b28cde9e69c31e3a5a27044ebd84847b543aa
gurbanovnihad <7404@holbertonstudents.com>
2024-02-29 22:53:57 +0400

    Base Model

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Base model defines all common attributes/methods for other classes
+"""
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Base model for other classes
+    """
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(type(self), self.id, self.__dict__)
+
+    def save(self):
+        """
+        save updates the public instance attribute updated_at with the current
+        datetime
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        to_dict returns a dictionary containing all keys/values of __dict__ of
+        the instance
+        """
+
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
