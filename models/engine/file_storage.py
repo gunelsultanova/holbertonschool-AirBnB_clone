bf0c9b8f7926d68b5b52c61453514502d86a0bce
gurbanovnihad <7404@holbertonstudents.com>
2024-03-02 21:15:59 +0400

    FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,7 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
