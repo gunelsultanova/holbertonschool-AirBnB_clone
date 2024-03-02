869decbf001d97e329ba29ef165378035bfd873c
gurbanovnihad <7404@holbertonstudents.com>
2024-03-02 22:06:01 +0400

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import cmd
+
+
+"""
+console.py contains the entry point of the command interpreter
+"""
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit the console."""
+        return True
+
+    def do_EOF(self, arg):
+        """Handle EOF (Ctrl+D on Unix/Linux, Ctrl+Z on Windows)."""
+        return True
+
+    def do_help(self, arg):
+        """Get help on commands.
+
+        Usage:
+            help [command]
+        """
+        if arg:
+            super().do_help(arg)
+        else:
+            super().do_help(None)
+
+    def emptyline(self):
+        """Do nothing on an empty line."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
